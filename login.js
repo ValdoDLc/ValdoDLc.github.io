b6619675352d157bbe384258ff6890db96dc7945
ValdoDlc <156165730+ValdoDLc@users.noreply.github.com>
2024-01-18 21:11:58 -0600

    login.js

--- login.js
+++ login.js
@@ -1,1 +1,6 @@
-document.addEventListener('DOMContentLoaded',function(){console.log("Hola BandaMax")});
+document.addEventListener('DOMContentLoaded',function(){console.log("Hola BandaMax");
+      var inputEmail = document.getElementbyId('email');
+      var inputPassword = document.getElementbyId('password');
+      console.log(inputEmail);
+      console.log(inputPassword);
+});
