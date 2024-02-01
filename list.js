ce105379812f8130b4bc9daf01cdb6032f3c0220
ValdoDlc <156165730+ValdoDLc@users.noreply.github.com>
2024-02-01 17:18:45 -0600

    list.js

--- list.js
+++ list.js
@@ -1,16 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "It's a new item";
-        itemList.appendChild(clone);
-    });
-    document.querySelector(".btn.btn-light").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        itemList.replaceChildren();
-    });
-});
+            let characterId = 1;
+            document.querySelector(".btn.btn-primary").addEventListener("click", event => {
+                fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        var itemList = document.getElementById("my-list");
+                        var template = document.getElementById("list-template");
+                        var total = itemList.childElementCount + 1;
+                        var clone = template.content.cloneNode(true);
+
+                        clone.querySelector("[data-id='number']").textContent = `${total}`;
+                        clone.querySelector("[data-id='title']").textContent = data.name;
+                        clone.querySelector("[data-id='content']").textContent = `Status: ${data.status}, Species: ${data.species}`;
+                        clone.querySelector("[data-id='image']").src = data.image;
+
+                        itemList.appendChild(clone);
+                        characterId++;
+                    })
+                    .catch(error => console.error('Error fetching data:', error));
+            });
+
+            document.querySelector(".btn.btn-light").addEventListener("click", event => {
+                var itemList = document.getElementById("my-list");
+                itemList.replaceChildren();
+            });
+        });
